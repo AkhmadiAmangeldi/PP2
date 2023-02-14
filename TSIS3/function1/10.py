877f610b5f92e88a108d657b68f20b07d656293c
AkhmadiAmangeldi <123235527+AkhmadiAmangeldi@users.noreply.github.com>
2023-02-14 17:06:33 +0600

    Update 10.py

---
+++ TSIS3/function1/10.py
@@ -0,0 +1,17 @@
+my_list = [1,2,3,2,4,5,5,4,6,7,5,8]
+
+def unique(my_list):
+    d = {}
+    l = []
+    for i in my_list:
+        if(i not in d):
+            d[i] = 1
+        else:
+            d[i] += 1
+
+    for x, y in d.items():
+        if(y == 1):
+            l.append(x)
+    return l
+
+print(unique(my_list))
